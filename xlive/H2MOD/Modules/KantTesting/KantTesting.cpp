d7359defb9521bbbf8d57bbb4d39f3a5b54eff60
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2024-04-23 21:27:57 -0500

    xml definition loader work
    initial loading of tag data from cache.

--- xlive/H2MOD/Modules/KantTesting/KantTesting.cpp
+++ xlive/H2MOD/Modules/KantTesting/KantTesting.cpp
@@ -5,6 +5,8 @@
 #include "Networking/logic/life_cycle_manager.h"
 #include "H2MOD/Modules/Input/KeyboardInput.h"
 #include "H2MOD/Modules/OnScreenDebug/OnscreenDebug.h"
+#include "H2MOD/Tags/MetaLoader/xml_loader.h"
+#include "tag_files/files_windows.h"
 
 
 namespace KantTesting
@@ -15,7 +17,13 @@ namespace KantTesting
 
 	void Initialize()
 	{
-		if (ENABLEKANTTEST) {
-		}
+		tag_group type;
+		type.group = _tag_group_biped;
+		c_xml_definition_agent lol(type, "C:\\Halo2\\mods\\plugins\\bipd.xml");
+		//c_xml_definition_agent lol(type, "C:\\Halo2\\mods\\plugins\\sky.xml");
+		FILE* a = fopen("C:\\Halo2\\mods\\maps\\carto_shared.map", "r");
+		c_xml_definition_loader heh(lol.get_definition(), a, 0xE29A00BF);
+		//c_xml_definition_loader heh(lol.get_definition(), a, 0xE19B001Du);
+		auto ab = 1233123;
 	}
 }
