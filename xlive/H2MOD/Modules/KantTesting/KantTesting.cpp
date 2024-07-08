24febc8a0d604d4f0c82f3cc557c516b7e0165cf
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2024-07-08 11:47:09 -0400

    convert path usage to use wide chars
    - add log function to c_xml_definition_agent

--- xlive/H2MOD/Modules/KantTesting/KantTesting.cpp
+++ xlive/H2MOD/Modules/KantTesting/KantTesting.cpp
@@ -17,15 +17,5 @@ namespace KantTesting
 
 	void Initialize()
 	{
-		tag_group type;
-		type.group = _tag_group_biped;
-
-		//c_tag_injecting_manager manager;
-
-		//manager.set_active_map("carto_shared");
-		//manager.load_tag(_tag_group_biped, "objects\\characters\\masterchief_skeleton\\masterchief_skeleton", true);
-
-
-		auto ab = 1233123;
 	}
 }
