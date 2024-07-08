d0adde3f469022f50f5e923744b91192ab22ea76
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2024-07-08 11:47:09 -0400

    convert path usage to use wide chars
    - add log function to c_xml_definition_agent

--- xlive/H2MOD/Modules/SpecialEvents/SpecialEvents.cpp
+++ xlive/H2MOD/Modules/SpecialEvents/SpecialEvents.cpp
@@ -14,6 +14,7 @@
 #include "H2MOD/Modules/Shell/Config.h"
 #include "tag_files/tag_loader/tag_injection.h"
 
+
 // Enables event if the current date and time line up with an event time
 e_special_event_type get_current_special_event()
 {
@@ -69,7 +70,7 @@ e_special_event_type get_current_special_event()
 
 void load_special_event()
 {
-	if (tag_injection_check_map_exists("carto_shared"))
+	if (tag_injection_check_map_exists(k_events_map))
 	{
 		bool create_new_markers = true;
 		switch (get_current_special_event())
