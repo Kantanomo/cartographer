eabb2b87fcded068bee5dabb886da789d81fb7be
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2024-11-27 20:05:07 -0600

    oddball

---
+++ xlive/Blam/Engine/game/game_engine_oddball.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "game_engine_default.h"
+
+class c_oddball_engine : c_game_engine
+{
+public:
+	virtual e_game_engine_type get_type() override;
+	virtual bool setup() override;
+	virtual void send_game_start_event(datum player_index) override;
+	virtual void function_13() override;
+	virtual void render_game_engine_elements(uint32 user_index) override;
+	virtual void function_16(datum player_index) override;
+	virtual void function_19() override;
+	virtual real32 get_player_speed_modifier(datum player_index) override;
+	virtual void update_object_color_change(datum object_index) override;
+	virtual void handle_object_taken_event(datum weapon_index, datum biped_index) override;
+	virtual void handle_object_dropped_event(datum weapon_index, datum biped_index) override;
+	virtual void get_multiplayer_score_string(wchar_t* out_string) override;
+	virtual void function_31(datum player_index, datum player_index_2, bool a3, int32 a4) override;
+	virtual void function_33(datum player_index, void* unk) override;
+	virtual bool function_35(int32 unk_index) override;
+	virtual bool test_variant_engine_flag(datum player_index, e_game_engine_variant_flag_test_type type) override;
+	virtual void get_player_state_index(datum player_index, bool* always_returned_true) override;
+	virtual uint32 get_game_engine_entity_type() override;
+	virtual void function_41() override;
+	virtual void set_simulation_baseline_data(int32 unused, void* unk) override;
+	virtual void build_simulation_update(void* unk, int32 unused, void* unk_2) override;
+	virtual bool apply_simulation_update(int16 flags, int32 unused, void* unk) override;
+};
