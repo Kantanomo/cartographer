09fbd5484ad0c3144c7970ac12d30ce3b5e9be2d
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2025-03-31 20:50:33 -0400

    organize first person camera file
    - add vector validation to first_person_camera_build_observer_command

--- xlive/Blam/Engine/camera/first_person_camera.cpp
+++ xlive/Blam/Engine/camera/first_person_camera.cpp
@@ -10,94 +10,21 @@
 #include "units/unit_definitions.h"
 #include "units/vehicle_definitions.h"
 
-/* private code */
-
-void __cdecl first_person_camera_build_observer_command(datum player_unit_index, real_vector3d* unit_facing, s_observer_command* observer_command)
-{
-	ASSERT(unit_facing);
-	ASSERT(observer_command);
-
-	observer_command->timer = 0;
-	observer_command->flags = 0;
-	observer_command->position.orientation = *global_zero_vector3d;
-	observer_command->focus_distance = 0.f;
-	observer_command->forward = *unit_facing;
-	observer_command->field_of_view = DEGREES_TO_RADIANS(70.f);
-	generate_up_vector3d(&observer_command->forward, &observer_command->up);
-	if(player_unit_index != NONE)
-	{
-		unit_datum* player_unit = (unit_datum*)object_get_fast_unsafe(player_unit_index);
-		unit_get_camera_position(player_unit_index, &observer_command->position.position);
-		object_get_velocities(player_unit_index, &observer_command->velocity, nullptr);
-		if(player_unit->object.parent_object_index)
-		{
-			unit_datum* vehicle_unit = (unit_datum*)object_try_and_get_and_verify_type(player_unit->object.parent_object_index, _object_mask_vehicle);
-			if(vehicle_unit)
-			{
-				struct vehicle_definition* vehicle_definition = (struct vehicle_definition*)tag_get_fast(vehicle_unit->definition_index);
-
-				if(vehicle_definition->unit.seats[player_unit->unit.parent_seat_index]->flags.test(_unit_seat_definition_first_person_camera_slaved_to_gun))
-				{
-					object_marker vehicle_marker{};
-					if(object_get_markers_by_string_id(player_unit->object.parent_object_index, (string_id)0xF0000DBu, &vehicle_marker, 1))
-					{
-						observer_command->position.position = vehicle_marker.matrix.position;
-						observer_command->forward = vehicle_marker.matrix.vectors.forward;
-						observer_command->up = vehicle_marker.matrix.vectors.up;
-					}
-					else
-					{
-						real_point3d parent_position{};
-						real_matrix4x3 parent_matrix{};
-						object_get_origin_interpolated(player_unit->object.parent_object_index, &parent_position);
-						matrix4x3_from_point_and_vectors(&parent_matrix, &parent_position, &vehicle_unit->object.forward, &vehicle_unit->object.up);
-						matrix4x3_inverse_transform_normal(&parent_matrix, &observer_command->forward, &observer_command->forward);
-						generate_up_vector3d(&observer_command->forward, &observer_command->up);
-						matrix4x3_transform_vector(&parent_matrix, &observer_command->forward, &observer_command->forward);
-						matrix4x3_transform_vector(&parent_matrix, &observer_command->up, &observer_command->up);
-					}
-				}
-			}
-		}
-		observer_command->flags = 1;
-	}
-	observer_validate_camera_command(observer_command);
-}
+/* typedefs */
 typedef void(__cdecl* t_first_person_camera_update)(int8* camera, s_director_update* director_update, s_observer_command* observer_command);
+
+/* globals */
+
+real32 g_camera_scale = 1.f;
+real32 g_observer_suggested_field_of_view = DEGREES_TO_RADIANS(70.f);
+
 t_first_person_camera_update p_first_person_camera_update;
 
-void __cdecl first_person_camera_update(int8* camera, s_director_update* director_update, s_observer_command* observer_command)
-{
-	ASSERT(camera);
-	ASSERT(director_update);
-	ASSERT(observer_command);
+/* prototypes */
 
-	datum player_unit_index = NONE;
-	if (players_user_is_active(director_update->user_index))
-		player_unit_index = s_player::get_unit_index(player_index_from_user_index(director_update->user_index));
+static void __cdecl first_person_camera_build_observer_command(datum player_unit_index, real_vector3d* unit_facing, s_observer_command* observer_command);
 
-	real_vector3d unit_facing{};
-
-	player_control_get_facing_direction(director_update->user_index, &unit_facing);
-	first_person_camera_build_observer_command(player_unit_index, &unit_facing, observer_command);
-
-	s_game_globals* globals = scenario_get_game_globals();
-	s_saved_game_cartographer_player_profile* player_profile = cartographer_player_profile_get_by_user_index(director_update->user_index);
-	observer_command->crosshair_position.x = 0.f;
-	observer_command->crosshair_position.y = player_profile->crosshair_offset;
-	real32 field_of_view = player_control_get_field_of_view(director_update->user_index) * 1.f;
-
-	field_of_view = PIN(field_of_view, DEGREES_TO_RADIANS(1.f), DEGREES_TO_RADIANS(110.f));
-
-	observer_command->field_of_view = field_of_view;
-
-	if(*(real32*)camera != field_of_view)
-	{
-		observer_command->observer_command_values[4] = 0.18f;
-		observer_command->observer_command_flags[4] = _observer_command_bit_1;
-		*(real32*)camera = field_of_view;
-	}
-}
+static void __cdecl first_person_camera_update(int8* camera, s_director_update* director_update, s_observer_command* result);
 
 /* public code */
 
@@ -105,5 +32,106 @@ void first_person_camera_apply_patches(void)
 {
 	// Patch this call for our custom fov implementation
 	DETOUR_ATTACH(p_first_person_camera_update, Memory::GetAddress<t_first_person_camera_update>(0xCD7E1), first_person_camera_update);
+	return;
+}
+
+/* private code */
+
+static void __cdecl first_person_camera_build_observer_command(datum player_unit_index, real_vector3d* unit_facing, s_observer_command* result)
+{
+	result->timer = 0;
+	result->flags = 0;
+	result->position.orientation = *global_zero_vector3d;
+	result->focus_distance = 0.f;
+	result->forward = *unit_facing;
+	result->field_of_view = g_observer_suggested_field_of_view;
+
+	generate_up_vector3d(&result->forward, &result->up);
+	assert_valid_real_vector3d_axes2(&result->forward, &result->up);
+
+	if (player_unit_index != NONE)
+	{
+		unit_datum* player_unit = (unit_datum*)object_get_fast_unsafe(player_unit_index);
+		unit_get_camera_position(player_unit_index, &result->position.position);
+		object_get_velocities(player_unit_index, &result->velocity, nullptr);
+		if(player_unit->object.parent_object_index)
+		{
+			unit_datum* vehicle_unit = (unit_datum*)object_try_and_get_and_verify_type(player_unit->object.parent_object_index, _object_mask_vehicle);
+			if(vehicle_unit)
+			{
+				struct vehicle_definition* vehicle_definition = (struct vehicle_definition*)tag_get_fast(vehicle_unit->definition_index);
+
+				if (vehicle_definition->unit.seats[player_unit->unit.parent_seat_index]->flags.test(_unit_seat_definition_first_person_camera_slaved_to_gun))
+				{
+					object_marker vehicle_marker;
+					if (object_get_markers_by_string_id(player_unit->object.parent_object_index, (string_id)0xF0000DBu, &vehicle_marker, 1))
+					{
+						result->position.position = vehicle_marker.matrix.position;
+						result->forward = vehicle_marker.matrix.vectors.forward;
+						result->up = vehicle_marker.matrix.vectors.up;
+					}
+					else
+					{
+						real_point3d parent_position;
+						real_matrix4x3 parent_matrix;
+						object_get_origin_interpolated(player_unit->object.parent_object_index, &parent_position);
+						matrix4x3_from_point_and_vectors(&parent_matrix, &parent_position, &vehicle_unit->object.forward, &vehicle_unit->object.up);
+						matrix4x3_inverse_transform_normal(&parent_matrix, &result->forward, &result->forward);
+						generate_up_vector3d(&result->forward, &result->up);
+						matrix4x3_transform_vector(&parent_matrix, &result->forward, &result->forward);
+						matrix4x3_transform_vector(&parent_matrix, &result->up, &result->up);
+					}
+				}
+			}
+		}
+		result->flags = 1;
+	}
+	observer_validate_camera_command(result);
+	return;
+}
+
+static void __cdecl first_person_camera_update(int8* camera, s_director_update* director_update, s_observer_command* result)
+{
+	ASSERT(camera);
+	ASSERT(director_update);
+	ASSERT(result);
+
+	datum player_unit_index = NONE;
+	if (players_user_is_active(director_update->user_index))
+	{
+		const datum player_index = player_index_from_user_index(director_update->user_index);
+		player_unit_index = ((s_player*)datum_get(s_player::get_data(), player_index))->unit_index;
+	}
+
+	real_vector3d unit_facing;
+	player_control_get_facing_direction(director_update->user_index, &unit_facing);
+	first_person_camera_build_observer_command(player_unit_index, &unit_facing, result);
+	
+	// New method to override the crosshair offset using carto profiles
+	if (true)
+	{
+		s_saved_game_cartographer_player_profile* player_profile = cartographer_player_profile_get_by_user_index(director_update->user_index);
+		result->crosshair_position.x = 0.f;
+		result->crosshair_position.y = player_profile->crosshair_offset;
+	}
+	// Original method of setting the crosshair position
+	else
+	{
+		const s_game_globals* globals = scenario_get_game_globals();
+		const s_game_globals_player_control* player_control = globals->player_control[0];
+		result->crosshair_position = player_control->crosshair_location;
+	}
+
+	real32 field_of_view = player_control_get_field_of_view(director_update->user_index) * g_camera_scale;
+	field_of_view = PIN(field_of_view, DEGREES_TO_RADIANS(1.f), DEGREES_TO_RADIANS(110.f));
+
+	result->field_of_view = field_of_view;
+
+	if(*(real32*)camera != field_of_view)
+	{
+		result->observer_command_values[4] = 0.18f;
+		result->observer_command_flags[4] = _observer_command_bit_1;
+		*(real32*)camera = field_of_view;
+	}
 	return;
 }
