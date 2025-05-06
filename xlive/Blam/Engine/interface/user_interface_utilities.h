f67203b2706239c8239b141389712e0b307d7af5
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2025-05-06 14:36:06 -0400

    fix mouse scrolling and selection in singleplayer splitscreen pause menu

--- xlive/Blam/Engine/interface/user_interface_utilities.h
+++ xlive/Blam/Engine/interface/user_interface_utilities.h
@@ -7,6 +7,8 @@ enum e_user_interface_global_sound;
 
 /* prototypes */
 
+void user_interface_utilities_apply_patches(void);
+
 datum __cdecl user_interface_get_widget_tag_index_from_screen_id(e_user_interface_screen_id screen_id);
 
 bool __cdecl user_interface_exit_to_dash();
@@ -20,5 +22,3 @@ void __cdecl user_interface_get_cursor_position(point2d* out_position);
 void __cdecl user_interface_get_cursor_position_scaled(point2d* out_position);
 
 void __cdecl user_interface_utilities_play_sound(e_user_interface_global_sound sound_type);
-
-void user_interface_utilities_apply_patches();
