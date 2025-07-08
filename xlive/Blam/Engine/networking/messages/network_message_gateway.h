9b3976b7399215df1fb5672355c532ba2f401692
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2025-07-08 15:57:20 -0400

    update c_network_message_gateway

--- xlive/Blam/Engine/networking/messages/network_message_gateway.h
+++ xlive/Blam/Engine/networking/messages/network_message_gateway.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "network_message_handler.h"
-#include "network_message_type_collection.h"
 #include "network_messages_out_of_band.h"
 
 #include "networking/delivery/network_link.h"
@@ -10,16 +8,16 @@
 class c_network_message_gateway : public c_network_out_of_band_consumer
 {
 public:
-	bool initialize_gateway(c_network_link* link, c_network_message_type_collection* message_types);
-	void attach_handler(c_network_message_handler* handler);
+	bool initialize_gateway(class c_network_link* link, class c_network_message_type_collection* message_types);
+	void attach_handler(class c_network_message_handler* handler);
 private:
 	bool m_initialized;
-	int8 pad0[3];
-	c_network_link* m_link;
-	c_network_message_type_collection* m_message_types;
-	c_network_message_handler* m_handler;
-	bool m_field_14;
-	int8 pad1[3];
-	int8 gap0[1608];
+	class c_network_link* m_link;
+	class c_network_message_type_collection* m_message_types;
+	class c_network_message_handler* m_handler;
+	bool m_outgoing_packet_pending;
+	int8 m_outgoing_packet_storage[k_network_link_maximum_game_data_size];
+	transport_address m_outgoing_packet_address;
+	c_bitstream m_outgoing_packet;
 };
 ASSERT_STRUCT_SIZE(c_network_message_gateway, 1632);
