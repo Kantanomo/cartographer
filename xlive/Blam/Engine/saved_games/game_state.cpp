7c2f3ac48a3fe0d5d869ec922423251182bb7f2c
nukeulater <18323679+nukeulater@users.noreply.github.com>
2025-10-26 13:28:13 +0200

    add game state globals
    added c_gamestate_allocation

--- xlive/Blam/Engine/saved_games/game_state.cpp
+++ xlive/Blam/Engine/saved_games/game_state.cpp
@@ -11,3 +11,8 @@ void* __cdecl game_state_malloc(const char* name, const char* description, uint3
 {
 	return INVOKE(0x2FF1D, 0x53327, game_state_malloc, name, description, size);
 }
+
+s_game_state_globals* game_state_globals_get()
+{
+	return Memory::GetAddress<s_game_state_globals*>(0x47A728, 0x4D2590);
+}
--- xlive/Blam/Engine/saved_games/game_state.h
+++ xlive/Blam/Engine/saved_games/game_state.h
@@ -1,38 +1,84 @@
 #pragma once
+
 #include "game/game_options.h"
+#include "memory/rockall_heap_manager.h"
 
 /* structures */
 
-struct s_game_state_header
+enum
 {
-	int alloc_checksum;
+	k_game_state_cpu_size = 0x3BE000,
+
+	// xbox specific when memory was unified?
+	k_game_state_gpu_size = 0x40000,
+
+	k_game_state_count = 2,
+};
+
+class c_gamestate_allocation : public c_allocation_base
+{
+public:
+	virtual void* alloc(size_t bytes);
+	virtual void* free_block(void* block);
+};
+
+struct game_state_header
+{
+	uint32 alloc_checksum;
 	void* base_address;
 	char scenario_name[256];
 	char game_build[32];
-	int map_checksum;
-	int field_12C;
+	uint32 map_checksum;
+	int32 field_12C;
 	s_game_options options;
-	int active_bsp_index;
-	int field_12BC;
-	int field_12C0;
-	int field_12C4;
-	int field_12C8;
-	int field_12CC;
-	int field_12D0;
-	int field_12D4;
-	int field_12D8;
-	int field_12DC;
-	int field_12E0;
-	int field_12E4;
-	int field_12E8;
-	int field_12EC;
-	int field_12F0;
-	int field_12F4;
+	int32 active_bsp_index;
+	int32 field_12BC;
+	int32 field_12C0;
+	int32 field_12C4;
+	int32 field_12C8;
+	int32 field_12CC;
+	int32 field_12D0;
+	int32 field_12D4;
+	int32 field_12D8;
+	int32 field_12DC;
+	int32 field_12E0;
+	int32 field_12E4;
+	int32 field_12E8;
+	int32 field_12EC;
+	int32 field_12F0;
+	int32 field_12F4;
 };
-ASSERT_STRUCT_SIZE(s_game_state_header, 4856);
+ASSERT_STRUCT_SIZE(game_state_header, 4856);
+
+struct s_game_state_globals
+{
+	bool initialized;
+	bool locked;
+
+	bool saved_game_valid;
+	bool fallback_valid;
+	int16 current_saved_game_index;
+	game_state_header game_saves[k_game_state_count];
+	uint32 game_time_at_last_save;
+	int16 number_of_bad_reverts;
+
+	void* base_address;
+	uint32 cpu_allocation_size;
+	uint32 gpu_allocation_size;
+	uint32 allocation_size_checksum;
+
+	uint32 revert_time;
+	game_state_header* header;
+
+	bool loaded_from_persistent_storage;
+	c_gamestate_allocation* gamestate_allocation;
+};
+ASSERT_STRUCT_SIZE(s_game_state_globals, 9760);
 
 /* prototypes */
 
 void __cdecl game_state_initialize(void);
 
 void* __cdecl game_state_malloc(const char* name, const char* description, uint32 size);
+
+s_game_state_globals* game_state_globals_get();
