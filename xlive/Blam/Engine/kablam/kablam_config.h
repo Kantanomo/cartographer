69450dafcf03d6066e0f34f525b807f2ab460513
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2025-12-16 23:13:25 -0600

    h2server live autosignin bypass
    removes the requirement for running the 'live autosignin' command for a server to startup.

--- xlive/Blam/Engine/kablam/kablam_config.h
+++ xlive/Blam/Engine/kablam/kablam_config.h
@@ -13,8 +13,22 @@ class c_kablam_config
 	wchar_t m_playlist_folder[MAX_PATH];
 };
 
+class c_kablam_config_live
+{
+public:
+	wchar_t live_id[256];
+	wchar_t description[32];
+	wchar_t owner[16];
+	int32 privacy_type;
+	bool enable_multiple_instances;
+	int32 override_port;
+	void* kablam_registry_key;
+};
+
 /* prototypes */
 
 void __cdecl kablam_config_initialize(HKEY key, LPCWSTR subkey);
+
+c_kablam_config_live* kablam_config_live_get();
 
 void kablam_config_apply_patches(void);
