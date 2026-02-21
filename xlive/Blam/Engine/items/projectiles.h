747f8f4b176f6907f443c26c8e3ac7c4aa044b03
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2026-02-21 14:40:38 -0600

    projectile_datum and scenery_datum changes

--- xlive/Blam/Engine/items/projectiles.h
+++ xlive/Blam/Engine/items/projectiles.h
@@ -1,1 +1,45 @@
 #pragma once
+#include "objects/objects.h"
+
+/* structures */
+
+struct _projectile_datum
+{
+	int32 flags;
+	int8 action;
+	int8 consecutive_collision_counter;
+	int16 hit_global_material;
+	real_vector3d hit_normal;
+	datum ignore_object_index;
+	datum target_object_index;
+	int32 target_model_target_index;
+	datum contrail_attachment_index;
+	int32 field_24;
+	real32 detonation_timer;
+	int32 field_2C;
+	real32 detonation_timer_delta;
+	real32 arming_time;
+	real32 arming_time_delta;
+	real32 odometer;
+	real32 velocity_scale;
+	real32 maximum_damage_distance;
+	real_vector3d rotation_axis;
+	real32 rotation_sine;
+	real32 rotation_cosine;
+	real32 damage_scale;
+	real32 guided_angular_velocity;
+	real32 last_thrust_speed;
+	real32 thrust_odometer;
+	real_point3d start_point;
+	int32 field_78;
+	uint16 shot_id;
+	uint16 time_of_flight_ticks;
+};
+
+struct projectile_datum
+{
+	datum definition_index;
+	_object_datum object;
+	_projectile_datum projectile;
+};
+ASSERT_STRUCT_SIZE(projectile_datum, 0x1AC);
