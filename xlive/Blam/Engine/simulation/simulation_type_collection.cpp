87c853550424305e852599242654648bf2218f8a
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-03-17 09:22:42 -0400

    c_simulation_type_collection work
    - add get_entity_definition and get_event_definition

--- xlive/Blam/Engine/simulation/simulation_type_collection.cpp
+++ xlive/Blam/Engine/simulation/simulation_type_collection.cpp
@@ -1,29 +1,50 @@
 #include "stdafx.h"
 #include "simulation_type_collection.h"
 
-c_simulation_entity_definition* c_simulation_type_collection::get_entity_definition(e_simulation_entity_type entity_type) const
+/* public code */
+
+c_simulation_entity_definition* c_simulation_type_collection::get_entity_definition(
+	e_simulation_entity_type entity_type) const
 {
-	return m_entity_definitions[entity_type];
+	ASSERT(entity_type>=0 && entity_type<m_entity_type_count);
+	
+	c_simulation_entity_definition* entity_definition = m_entity_definitions[entity_type];
+	ASSERT(entity_definition !=NULL);
+	ASSERT(entity_definition->entity_type()==entity_type);
+
+	return entity_definition;
 }
 
-c_simulation_event_definition* c_simulation_type_collection::get_event_definition(e_simulation_event_type event_type) const
+c_simulation_event_definition* c_simulation_type_collection::get_event_definition(
+	e_simulation_event_type event_type) const
 {
-	return m_event_definitions[event_type];
+	ASSERT(event_type>=0 && event_type<m_event_type_count);
+
+	c_simulation_event_definition* event_definition = m_event_definitions[event_type];
+	ASSERT(event_definition!=NULL);
+	ASSERT(event_definition->event_type()==event_type);
+
+	return event_definition;
 }
 
-int32 c_simulation_type_collection::get_event_definition_count(void) const
+int32 c_simulation_type_collection::get_event_definition_count(
+	void) const
 {
 	ASSERT(m_event_type_count >= 0);
 	return m_event_type_count;
 }
 
-void c_simulation_type_collection::register_entity_definition(e_simulation_entity_type entity_type, c_simulation_entity_definition* definition)
+void c_simulation_type_collection::register_entity_definition(
+	e_simulation_entity_type entity_type,
+	c_simulation_entity_definition* definition)
 {
 	m_entity_definitions[entity_type] = definition;
 	return;
 }
 
-void c_simulation_type_collection::register_event_definition(e_simulation_event_type type, c_simulation_event_definition* definition)
+void c_simulation_type_collection::register_event_definition(
+	e_simulation_event_type type,
+	c_simulation_event_definition* definition)
 {
 	m_event_definitions[type] = definition;
 	return;
