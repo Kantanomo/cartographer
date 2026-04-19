5807cd3da7223989060d5b1599aaf29dd390d6f2
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-04-19 00:19:59 -0400

    c_language_pack work
    - add unload_data function

--- xlive/Blam/Engine/text/text_group.cpp
+++ xlive/Blam/Engine/text/text_group.cpp
@@ -4,62 +4,95 @@
 #include "main/game_preferences.h"
 #include "game/game_globals.h"
 
-
-
 /* structures */
+
 struct s_unicode_string_list_reference
 {
 	uint16 strings_index;
 	uint16 strings_count;
 };
 
-
 /* public code */
 
-bool c_language_pack::try_find_string_exists(string_id id, int32 starting_index, int32 max_count)
+bool c_language_pack::unload_data(
+	void)
+{
+	if (m_data_loaded)
+	{
+		CSERIES_FREE(m_string_references);
+		CSERIES_FREE(m_string_data);
+		m_string_references = NULL;
+		m_string_data = NULL;
+		m_data_loaded = false;
+	}
+
+	return true;
+}
+
+bool c_language_pack::try_find_string_exists(
+	string_id id,
+	int32 starting_index,
+	int32 max_count)
 {
 	return INVOKE_TYPE(0x3DF4C, 0x0, bool(__thiscall*)(c_language_pack*, string_id, int32, int32), this, id, starting_index, max_count);
 }
 
-utf8* c_language_pack::get_string_utf8(string_id id, int32 starting_index, int32 max_count)
+utf8* c_language_pack::get_string_utf8(
+	string_id id,
+	int32 starting_index,
+	int32 max_count)
 {
 	return INVOKE_TYPE(0x3DEFD, 0x0, utf8*(__thiscall*)(c_language_pack*, string_id, int32, int32), this, id, starting_index, max_count);
 }
 
-const int32 c_language_pack::get_num_of_strings() const
-{
-	ASSERT(m_strings_count < k_max_strings_per_language);
-
-	return m_strings_count;
-}
-
-void c_language_pack::string_list_get_normal_string(string_id id, c_maximum_interface_text* out_string, int32 strings_start_index, int32 strings_count)
+void c_language_pack::string_list_get_normal_string(
+	string_id id,
+	c_maximum_interface_text* out_string,
+	int32 strings_start_index,
+	int32 strings_count)
 {
 	INVOKE_TYPE(0x3E332, 0x0, void(__thiscall*)(c_language_pack*, string_id, c_maximum_interface_text*, int32, int32), this, id, out_string, strings_start_index, strings_count);
+
+	return;
 }
 
-void c_language_pack::get_string_ids(string_id* out_ids, int32 out_count, int32 starting_index, int32 max_count)
+void c_language_pack::get_string_ids(
+	string_id* out_ids,
+	int32 out_count,
+	int32 starting_index,
+	int32 max_count)
 {
 	csmemset(out_ids, _string_id_invalid, sizeof(*out_ids) * out_count);
-	if (data_loaded)
+	
+	if (m_data_loaded)
 	{
 		for (int32 itr = starting_index; itr < max_count + starting_index; itr++)
 		{
 			int32 index = itr - starting_index;
 			if (index < out_count)//prevent buffer overrun
 			{
-				out_ids[index] = this->m_string_references[itr].string_id;
+				out_ids[index] = m_string_references[itr].string_id;
 			}
 		}
 	}
+
+	return;
 }
 
-void __cdecl string_list_get_normal_string(datum unic_datum, string_id id, c_maximum_interface_text* out_string)
+void __cdecl string_list_get_normal_string(
+	datum unic_datum,
+	string_id id,
+	c_maximum_interface_text* out_string)
 {
 	INVOKE(0x3E3AC, 0x0, string_list_get_normal_string, unic_datum, id, out_string);
+
+	return;
 }
 
-void __cdecl string_list_get_string_id_list(datum unic_datum, string_id* out_ids, int32 out_count)
+void __cdecl string_list_get_string_id_list(
+	datum unic_datum,
+	string_id* out_ids,
+	int32 out_count)
 {
 	ASSERT(out_ids);
 	ASSERT(out_count > 0);
@@ -73,4 +106,6 @@ void __cdecl string_list_get_string_id_list(datum unic_datum, string_id* out_ids
 		s_game_globals* game_globals = scenario_get_game_globals();
 		game_globals->language_pack[current_language].get_string_ids(out_ids, out_count, list_reference->strings_index, list_reference->strings_count);
 	}
+
+	return;
 }
--- xlive/Blam/Engine/text/text_group.h
+++ xlive/Blam/Engine/text/text_group.h
@@ -1,6 +1,11 @@
 #pragma once
 
-#define k_max_strings_per_language 0x8000
+/* constants */
+
+enum
+{
+	k_max_strings_per_language = 0x8000
+};
 
 /* structures */
 
@@ -10,29 +15,33 @@ struct s_string_reference
 	uint32 buffer_offset;
 };
 
+/* classes */
+
 // TODO add member functions
 class c_language_pack
 {
 public:
+	bool unload_data(void);
+	
 	bool try_find_string_exists(string_id id, int32 starting_index, int32 max_count);
 	utf8* get_string_utf8(string_id id, int32 starting_index, int32 max_count);
-	const int32 get_num_of_strings() const;
 	
 	void string_list_get_normal_string(string_id id, c_maximum_interface_text* out_string, int32 strings_start_index, int32 strings_count);
 	void get_string_ids(string_id* array, int32 array_size, int32 starting_index, int32 max_count);
 
+
 private:
 	s_string_reference* m_string_references;
-	char* m_strings_buffer;					// this is utf8 string data
-	int32 m_strings_count;
+	utf8* m_string_data;
+	int32 m_num_of_strings;
 	int32 m_string_data_size;
 	int32 m_string_reference_cache_offset;
 	int32 m_string_data_cache_offset;
-	bool data_loaded;
+	bool m_data_loaded;
 };
 ASSERT_STRUCT_SIZE(c_language_pack, 28);
 
-/* public code */
+/* prototypes */
 
 void __cdecl string_list_get_normal_string(datum unic_datum, string_id id, c_maximum_interface_text* out_string);
 void __cdecl string_list_get_string_id_list(datum unic_datum, string_id* out_ids, int32 out_count);
