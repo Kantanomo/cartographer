0e5c32a0d9573a9f99e7b629b01de2b2abceaaee
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-04-19 00:29:26 -0400

    add screen_postgame_statistics_finalize

---
+++ xlive/Blam/Engine/interface/screens/screen_postgame_statistics.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "screen_postgame_statistics.h"
+
+/* public code */
+
+void __cdecl screen_postgame_statistics_finalize(
+	void)
+{
+	INVOKE(0xC8CE, 0x2286A, screen_postgame_statistics_finalize);
+
+	return;
+}
