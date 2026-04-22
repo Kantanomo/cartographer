6ca55ebc5b8122b3b48b0dc1546fd01f353ca4e7
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-04-21 21:55:29 -0400

    collision bsp definition changes
    - uses accurate names and types

--- xlive/Blam/Engine/physics/collision_bsp_definition.h
+++ xlive/Blam/Engine/physics/collision_bsp_definition.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "tag_files/tag_block.h"
 
 /* constants */
 
@@ -26,9 +25,10 @@ enum e_collision_surface_flags
 	NUMBER_OF_COLLISION_SURFACE_FLAGS
 };
 
-enum e_collision_leaf_flags : uint8
+enum e_collision_leaf_flags
 {
-	_collision_leaf_contains_double_sided_surfaces = FLAG(0)
+	_collision_leaf_contains_two_sided_bit = 0,
+	NUMBER_OF_COLLISION_LEAF_FLAGS
 };
 
 /* structures */
@@ -39,35 +39,37 @@ struct bsp3d
 	s_tag_block planes;
 };
 
+struct bsp2d
+{
+	s_tag_block nodes;
+};
+
 // max: MAXIMUM_VERTICES_PER_COLLISION_BSP
 struct collision_vertex
 {
 	real_point3d point;
-	uint16 first_edge;
-	int16 pad;
+	uint16 first_edge_index;
+	int16 sink;
 };
 ASSERT_STRUCT_SIZE(collision_vertex, 16);
 
 // max: MAXIMUM_EDGES_PER_COLLISION_BSP
 struct collision_edge
 {
-	uint16 start_vertex;
-	uint16 end_vertex;
-	uint16 forward_edge;
-	uint16 reverse_edge;
-	uint16 left_surface;
-	uint16 right_surface;
+	uint16 vertex_indices[2];
+	uint16 edge_indices[2];
+	int16 surface_indices[2];
 };
 ASSERT_STRUCT_SIZE(collision_edge, 12);
 
 // max: MAXIMUM_SURFACES_PER_COLLISION_BSP
 struct collision_surface
 {
-	uint16 plane;
-	uint16 first_edge;
+	int16 plane_designator;
+	uint16 first_edge_index;
 	uint8 /*e_collision_surface_flags*/ flags;
-	int8 breakable_surface;
-	uint16 material;
+	int8 breakable_surface_index;
+	int16 material_index;
 };
 ASSERT_STRUCT_SIZE(collision_surface, 8);
 
@@ -75,32 +77,33 @@ ASSERT_STRUCT_SIZE(collision_surface, 8);
 struct bsp2d_node
 {
 	real_plane2d plane;
-	uint16 left_child;
-	uint16 right_child;
+	int16 child_indices[2];
 };
 ASSERT_STRUCT_SIZE(bsp2d_node, 16);
 
 // max: MAXIMUM_BSP2D_REFERENCES_PER_COLLISION_BSP
 struct bsp2d_reference
 {
-	uint16 plane;
-	uint16 bsp_2d_node;
+	int16 plane_designator;
+	int16 root_index;
 };
 ASSERT_STRUCT_SIZE(bsp2d_reference, 4);
 
 // max: MAXIMUM_LEAVES_PER_BSP3D
 struct collision_leaf
 {
-	e_collision_leaf_flags flags;
+	uint8 /*e_collision_leaf_flags*/ flags;
 	int8 bsp_2d_reference_count;
-	uint16 first_bsp_2d_reference;
+	uint16 first_bsp2d_reference_index;
 };
 ASSERT_STRUCT_SIZE(collision_leaf, 4);
 
 // max: MAXIMUM_NODES_PER_BSP3D
 struct bsp3d_node
 {
-	int8 data[8];
+	int64 plane_index : 16;
+	int64 below_child_index : 24;
+	int64 above_child_index : 24;
 };
 ASSERT_STRUCT_SIZE(bsp3d_node, 8);
 
@@ -108,11 +111,13 @@ struct collision_bsp
 {
 	bsp3d bsp3d;
 
-	tag_block<collision_leaf> leaves;
-	tag_block<bsp2d_reference> bsp_2d_references;
-	tag_block<bsp2d_node> bsp_2d_nodes;
-	tag_block<collision_surface> surfaces;
-	tag_block<collision_edge> edges;
-	tag_block<collision_vertex> vertices;
+	s_tag_block leaves;
+	s_tag_block bsp_2d_references;
+	
+	bsp2d bsp2d;
+
+	s_tag_block surfaces;
+	s_tag_block edges;
+	s_tag_block vertices;
 };
 ASSERT_STRUCT_SIZE(collision_bsp, 64);
