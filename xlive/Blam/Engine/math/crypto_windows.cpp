8d5b6982869607e2d41798ccf267cae3ac47df9d
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-06-16 17:53:51 -0400

    add crypto_windows_sha256_hash_data

---
+++ xlive/Blam/Engine/math/crypto_windows.cpp
@@ -0,0 +1,63 @@
+#include "stdafx.h"
+#include "crypto_windows.h"
+
+#include "networking/network_event.h"
+
+/* constants */
+
+static const wchar_t k_crypto_provider[] = L"Microsoft Enhanced RSA and AES Cryptographic Provider";
+
+/* public code */
+
+bool crypto_windows_sha256_hash_data(
+	void const *data,
+	uint32 size,
+	s_sha256_hash* hash)
+{
+	HCRYPTPROV provider = NULL;
+	HCRYPTHASH hash_handle = NULL;
+	uint32 hash_length = k_sha256_hash_size_bytes;
+	bool success = false;
+
+	if (!CryptAcquireContextW(&provider, NULL, k_crypto_provider, PROV_RSA_AES, CRYPT_VERIFYCONTEXT | CRYPT_SILENT))
+	{
+		error(_error_delayed, "CryptAcquireContext failed: 0x%x", GetLastError());
+	}
+	else if (!CryptCreateHash(provider, CALG_SHA_256, 0, 0, &hash_handle))
+	{
+		error(_error_delayed, "CryptCreateHash failed: 0x%x", GetLastError());
+	}
+	else if (!CryptHashData(hash_handle, (uint8 const*)data, size, 0))
+	{
+		error(_error_delayed, "CryptHashData failed: 0x%x", GetLastError());
+	}
+	else if (!CryptGetHashParam(hash_handle, HP_HASHVAL, hash->data, &hash_length, 0))
+	{
+		error(_error_delayed, "CryptGetHashParam failed: 0x%x", GetLastError());
+	}
+	else
+	{
+		success = true;
+
+		ASSERT(hash_length == sizeof(*hash));
+
+		// Originally logged with error but changed to verbose event so we don't have it spammed constantly
+		//error(_error_delayed, "Successfully hashed %d bytes", size);
+
+		event(_event_verbose, "Successfully hashed %d bytes", size);
+
+	}
+	
+	if (hash_handle)
+	{
+		CryptDestroyHash(hash_handle);
+		hash_handle = NULL;
+	}
+	
+	if (provider)
+	{
+		CryptReleaseContext(provider, 0);
+	}
+	
+	return success;
+}
--- xlive/Blam/Engine/math/crypto_windows.h
+++ xlive/Blam/Engine/math/crypto_windows.h
@@ -6,3 +6,14 @@ enum
 {
 	k_sha256_hash_size_bytes = 32,
 };
+
+/* structures */
+
+struct s_sha256_hash
+{
+	uint8 data[k_sha256_hash_size_bytes];
+};
+
+/* prototypes */
+
+bool crypto_windows_sha256_hash_data(void const *data, uint32 size, s_sha256_hash *hash);
