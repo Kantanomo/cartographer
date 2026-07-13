ca28c660b47ab0883d0f2e57362a02bb5d79588a
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-07-13 12:54:53 -0400

    use invoke macro in game_engine_util

--- xlive/Blam/Engine/game/game_engine_util.h
+++ xlive/Blam/Engine/game/game_engine_util.h
@@ -2,11 +2,11 @@
 
 /* public code */
 
-bool game_engine_in_round();
+bool game_engine_in_round(void);
 
-void game_engine_check_for_round_winner();
-void game_engine_end_round_with_winner(int player_datum_or_team_index, bool go_to_next_round);
-bool game_engine_has_teams();
+void __cdecl game_engine_check_for_round_winner(void);
+void __cdecl game_engine_end_round_with_winner(int32 player_datum_or_team_index, bool go_to_next_round);
+bool game_engine_has_teams(void);
 
 // ### TODO: function name
 bool __cdecl sub_4701B6(datum player_index);
